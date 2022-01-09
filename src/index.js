50a47054242790847cb2a7ca410e191eb6dfaa17
shinyangdevelop <shinyangdevelop@gmail.com>
2022-01-09 21:58:55 +0900

    Install @discordjs/builders @discordjs/rest discord-api-types, and wrote basic code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const { Client, Intents, Collection } = require('discord.js')
 const { token } = require('secret.json')
 
-const client = new Client()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+
+
+client.login(token)
