df9df2deb6117a8746a959d0211e151c8922e694
shinyangdevelop <shinyangdevelop@gmail.com>
2022-01-14 00:07:22 +0900

    Changed npm system to yarn system, and build basic features

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,8 +1,24 @@
+const { blocked_words } = require('../../config.json')
+
 module.exports = {
     name: "messageCreate",
     once: false,
     execute: async (message) => {
+        if(message.author.bot) {
+            return
+        }
+        let isblocked = false
         console.log(message.content)
-        await message.reply("Test!")
+        for(let counter=0; counter<blocked_words.length; counter+=1) {
+            if(message.content.toLowerCase().includes(blocked_words[counter])) {
+                isblocked = true
+            }
+        }
+        if(isblocked) {
+            message.delete().then(async msg => {
+                console.log(`Deleted message from ${msg.author.username}`)
+                await message.channel.send(`Blocked message from ${message.author.username} because it contained blocked word`)
+            })
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ const { Client, Intents, Collection } = require('discord.js')
 const { token } = require('../secret.json')
 
 const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'))
-const client = new Client({ intents: [Intents.FLAGS.GUILDS], partials: ["CHANNEL"]})
-
-client.on("message", message => {
-    console.log(message.content)
-})
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], partials: ["CHANNEL", "MESSAGE"]})
 
 for(const file of eventFiles) {
     const event = require(`./events/${file}`)
     if(event.once) {
         client.once(event.name, (...args) => event.execute(...args))
-        console.log(`Registered event ${event.name}`)
+        console.log(`Registered event ${event.name}, which is once`)
     }
     else {
         client.on(event.name, (...args) => event.execute(...args))
-        console.log(`Registered event ${event.name}`)
+        console.log(`Registered event ${event.name}, which is not once`)
     }
 }
 
